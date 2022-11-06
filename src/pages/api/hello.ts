360d152c9a3562ad1b4ff45e6d6755887d345a1f
Marty McGee <support@companyjuice.com>
2022-11-06 05:14:58 -0800

    v0.8.0a0 ThreeDGarden 🌱 Ethereum

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next"
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: "Marty McGee -||- HEY HEY HEY" })
+const handler = (req: NextApiRequest, res: NextApiResponse<Data>): void => {
+  res.status(200).json({ name: 'Marty McGee -||- HEY HEY HEY' })
 }
+
+export default handler
